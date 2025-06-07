a80ea0a5adf6bbc8668c21f21cf3caa34bfdfc95
Alex Samm <alexsamm231@hotmail.com>
2025-06-07 18:23:00 -0400

    Update 1-syntax.py

--- exercises/1-syntax.py
+++ exercises/1-syntax.py
@@ -1,5 +1,9 @@
-# Oh the syntax errors
-
+'''
+Syntax errors
+1. Copy the script into a new python file and give a name.
+2. Run it and see the errors generated.
+3. Fix it and run it again
+'''
 print("He ran-som-ware')
 
 print['Was it crypto"]
