fc8bd7cf554535952556c6535289be5eb4972868
Alex Samm <alexsamm231@hotmail.com>
2025-06-07 18:24:29 -0400

    Update 2-syntax.py

--- exercises/2-syntax.py
+++ exercises/2-syntax.py
@@ -1,11 +1,15 @@
-#Indentation errors
-
-def add(a,b):
+'''
+Indentation and Syntax errors
+1. Copy the code into a new python file and run
+2. Run it and see the errors
+3. Fix these and re-run
+'''
+a = 10
+b - 20
 print(a + b)
 
 day = "mon"
 
-    if day == "tues":
     print("today is not monday")
 
 
