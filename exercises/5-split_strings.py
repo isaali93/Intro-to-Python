829fb5db5d2b38cd7008d31dad897d82e7c95296
Alex Samm <alexsamm231@hotmail.com>
2025-06-07 22:53:47 -0400

    Create 5-split_strings.py

---
+++ exercises/5-split_strings.py
@@ -0,0 +1,8 @@
+'''
+1. The variable url has a string of web addresses
+2. Split the string to get the individual urls
+3. Print the 3rd url in the list
+4. The last url looks like it got translated in the wrong direction. Print it reversed.
+'''
+
+urls = "www.google.com,wwww.facebook.com/login,www.tiktok.com,www.coinmarketcap.com,moc.margatsni.www"
