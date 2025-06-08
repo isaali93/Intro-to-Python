6831e05976a9f8931dd00d0396cdf10c6cf21824
Alex Samm <alexsamm231@hotmail.com>
2025-06-07 22:24:15 -0400

    Create 3-return_ip.py

---
+++ exercises/3-return_ip.py
@@ -0,0 +1,19 @@
+'''
+1. Create a variable that has the current year
+2. Write a python script that asks the user their name and location.
+3. Print to the screen (on separate lines)
+  a. Their name in Title format and Location in Uppercase
+  b. Prompt the user for their gender and print the first character only
+  c. Ask the user for their birth year and print their current age to the screen
+'''
+
+'''
+Tips:
+- To use the TItle format use .title()
+- For uppercase format, .upper()
+- Use string slicing
+- Use variables to store your calculation
+'''
+
+# Here is the first variable done for you
+CURRENT_YEAR = 2025
