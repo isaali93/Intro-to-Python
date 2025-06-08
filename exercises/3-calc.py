2e080dc9ee90198c88e6a75e20457a06aa39e223
Alex Samm <alexsamm231@hotmail.com>
2025-06-07 22:31:29 -0400

    Create 3-calc.py

---
+++ exercises/3-calc.py
@@ -0,0 +1,7 @@
+'''
+1. Create 3 variables and assign them 3 integers of your choice
+2. Create a variable to store the sum of all the numbers
+3. Create a varaible to store the first number multiplied by the second then all divided by the third
+4. Create a variable to store the first number raised to the power of the third number, then subtract the second
+5. Print all results using a single print statement
+'''
