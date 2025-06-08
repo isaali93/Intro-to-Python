ca4e0bd5da2aa4434687ef128d760d926c1e8b2e
Alex Samm <alexsamm231@hotmail.com>
2025-06-07 22:58:52 -0400

    Update 5-split_strings.py

--- exercises/5-split_strings.py
+++ exercises/5-split_strings.py
@@ -5,4 +5,11 @@
 4. The last url looks like it got translated in the wrong direction. Print it reversed.
 '''
 
+'''
+TIP
+- Use .split()
+- String slicing can be combined with the .split() function
+- Rememeber the first value is at position 0
+'''
+
 urls = "www.google.com,wwww.facebook.com/login,www.tiktok.com,www.coinmarketcap.com,moc.margatsni.www"
