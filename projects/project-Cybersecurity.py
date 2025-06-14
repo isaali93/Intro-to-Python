808bec819f77c610d12933b115f79f1e4f5fc7b0
Alex Samm <alexsamm231@hotmail.com>
2025-06-14 12:57:27 -0400

    Update project-Cybersecurity.py

--- projects/project-Cybersecurity.py
+++ projects/project-Cybersecurity.py
@@ -9,3 +9,11 @@ Create a simple program that:
     - Stores these credentials in variables
     - Displays the stored information back to the user
 """
+
+# Week 2
+"""
+Add the following Features:
+    - Add password strength checking (length, special characters, numbers)
+    - Implement conditional feedback on password strength
+    - Allow user to decide whether to save the password based on strength
+"""
