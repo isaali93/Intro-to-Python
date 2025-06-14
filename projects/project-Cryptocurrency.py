4e159ed7a45cb6c57696ca566f4f47b49cb1c719
Alex Samm <alexsamm231@hotmail.com>
2025-06-14 12:59:27 -0400

    Update project-Cryptocurrency.py

--- projects/project-Cryptocurrency.py
+++ projects/project-Cryptocurrency.py
@@ -9,3 +9,14 @@ Create a simple program that:
     - Calculates the total value of the holding
     - Displays a simple summary of the user's crypto asset
 """
+
+# Week 2
+"""
+Add these features to this project
+    - Add current price input for the cryptocurrency
+    - Calculate profit/loss in dollar amount and percentage
+    - Implement conditional statements to:
+        1. Indicate if the investment is profitable or at a loss
+        2. Provide different messages based on profit/loss thresholds
+        3. Flag significant gains or losses with warnings or celebrations
+"""
