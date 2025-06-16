9edf2291034ea07837951b3e7dec2a7cf8406a39
Alex Samm <alexsamm231@hotmail.com>
2025-06-16 16:24:16 -0400

    Create 2-4 for2.py

---
+++ exercises/2-4 for2.py
@@ -0,0 +1,14 @@
+'''
+FizzBuzz is a common question asked in programming interviews
+
+Write a program that prints the following:
+  1. It should print from numbers 1 - 100
+  2. If the number is a multiple of 3: Replace the number with the word "Fizz"
+  3. If the number is a multiple of 5: Replace the number with the word "Buzz"
+  4. If the number is a multiple of both 3 and 5 (such as 15, and 30): Replace the number with the word "FizzBuzz"
+
+TIPS
+  - Use the range() function
+  - Use the % operator
+  - The range() function includes the starting number but excludes the end number
+'''
