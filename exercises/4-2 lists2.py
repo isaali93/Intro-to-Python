291b5a91105eaf886e049481bb2c073a5afad5b1
Alex Samm <alexsamm231@hotmail.com>
2025-06-30 16:11:10 -0400

    Create 4-2 lists2.py

---
+++ exercises/4-2 lists2.py
@@ -0,0 +1,7 @@
+"""
+Write a program to allow a user to add an item to the list
+1. The program should impletement an add_item function
+2. The program should exit when the word exit is entered
+"""
+
+user_group = ["Administrator","Domain Administrator","DB Admin","SQL Admin","General Users"]
