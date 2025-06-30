17b2721ec6007f0a916b219c846fd16cad4efddd
Alex Samm <alexsamm231@hotmail.com>
2025-06-30 16:34:25 -0400

    Create 5-1 modules.py

---
+++ exercises/5-1 modules.py
@@ -0,0 +1,13 @@
+"""
+Use the deck from exercise "4-3 lists.py"
+
+1. Import the random module
+2. Write a function to shuffle the deck
+3. Print the deck before the shuffle and after the shuffle
+
+TIP
+- The shuffle() function from the random module
+"""
+
+suits = ["Hearts","Spades","Clubs","Diamonds"]
+faces = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
