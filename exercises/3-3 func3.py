d1b1429358a048aa962ecca0bbeba46fe285a622
Alex Samm <alexsamm231@hotmail.com>
2025-06-30 15:45:48 -0400

    Create 3-3 func3.py

---
+++ exercises/3-3 func3.py
@@ -0,0 +1,7 @@
+"""
+Write a word counter
+1. The program should prompt the user for a sentence
+2. The program should exit when the user enters the word 'quit' or 'exit'
+3. There should be a function that returns the number of words in the sentence
+4. Print the result to the screen
+"""
