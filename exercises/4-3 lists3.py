8536e717130d4351f9159a590eb52233a53cf1dd
Alex Samm <alexsamm231@hotmail.com>
2025-06-30 16:23:14 -0400

    Update 4-3 lists3.py

--- exercises/4-3 lists3.py
+++ exercises/4-3 lists3.py
@@ -3,10 +3,15 @@ Here is a template program to work with.
 You have been given a function that generates a deck of cards and returns it as a list
 
 Write a program that "deals" 6 cards to a player.
+
+TIP
+- Use the player_hand variable to store the cards
+- Use the pop() function to "deal" the cards to Player
 """
 
 suits = ["Hearts","Spades","Clubs","Diamonds"]
 faces = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
+player_hand = []
 
 def build_deck(s,f):
   deck = []
