63bd83886ff7d31f3984670f7656be24b46597d6
Alex Samm <alexsamm231@hotmail.com>
2025-06-30 16:00:31 -0400

    Create 3-4 func4.py

---
+++ exercises/3-4 func4.py
@@ -0,0 +1,8 @@
+"""
+Write a program the gets the longest word in a sentence
+1. Create a variable that has a sentence
+2. Prompt the user to enter a sentence
+3. Create a function that takes the sentence as its input
+4. The function should analyze the sentence and return the longest word in the sentence
+5. Exit the program if the only word entered by the user is quit
+"""
