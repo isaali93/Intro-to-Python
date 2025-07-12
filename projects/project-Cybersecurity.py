48abd5c107c7de6ab0943cd44276439419067c16
Alex Samm <alexsamm231@hotmail.com>
2025-07-12 11:57:15 -0400

    Update project-Cybersecurity.py

--- projects/project-Cybersecurity.py
+++ projects/project-Cybersecurity.py
@@ -25,3 +25,22 @@ Include the following to your program:
     - Store multiple sets of credentials in a list of program/service:password pair (eg. google.com:password)
     - Implement features to add, view, and search for credentials
 """
+
+# Week 4
+"""
+Add the following to your program:
+    - Implement saving/loading credentials to/from a file
+    - Add some simple obfuscation (not true encryption yet and will require a little research)
+    - Create a master password to access the application
+    - Implement basic logging of access attempts
+
+Additional actions
+    - Refactor code into organized functions
+    - Implement password hashing for the master password
+    - Add simple encryption for stored passwords
+    - Create functions for security auditing (checking for weak/duplicate passwords)
+    - Implement some exception handling
+    - Create secure backup/restore functionality
+    - Add option to securely delete individual credentials
+"""
+
